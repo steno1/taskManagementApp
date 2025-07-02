f87c324ab87793adc62f297973c5eb318d0d65f4
Onu Princeley Toochukwu <onuprinceley@gmail.com>
2025-07-02 10:29:06 +0100

    Update NavLinks.js

--- client/src/components/NavLinks.js
+++ client/src/components/NavLinks.js
@@ -7,20 +7,20 @@ const NavLinks = () => {
   const { toggleSideBar } = useAppContext();
   return (
     <div className='nav-links'>
-   {/* Map through the links array and render NavLink for each link */}
+
      {links.map((link) => {
      const { text, path, id, icon } = link;
       return (
-      // Navigation link with the link's text, icon, and onClick handler
+    
       <NavLink
-      to={path} // Set the link's destination URL
-     key={id} // Use the link's id as the unique key for efficient rendering
-      onClick={toggleSideBar} // Call toggleSideBar function when a link is clicked to close the sidebar
-      activeclassname='active' // Apply the 'active' class when the link's URL matches the current URL
-      className='nav-link' // Set the default class for all navigation links
+      to={path} 
+     key={id} 
+      onClick={toggleSideBar} 
+      activeclassname='active' 
+      className='nav-link'
         >
-        <span className='icon'>{icon}</span> {/* Display the link's icon */}
-          {text} {/* Display the link's text */}
+        <span className='icon'>{icon}</span> 
+          {text} 
           </NavLink>
               );
             })}
