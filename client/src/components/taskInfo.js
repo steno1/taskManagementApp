80f0636a33bac4de79c924230eb8b086663ac488
Onu Princeley Toochukwu <onuprinceley@gmail.com>
2025-07-02 14:13:38 +0100

    Update taskInfo.js

--- client/src/components/taskInfo.js
+++ client/src/components/taskInfo.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import Wrapper from '../assets/wrappers/taskInfo';
 
-// Import the React library
- // Import the Wrapper component
-
-// Define the TaskInfo component
 const TaskInfo = ({ icon, text, Description }) => {
   return (
     <Wrapper>
-      {/* Render the provided icon */}
       <span className='icon'>{icon}</span>
-      {/* Render the provided text */}
       <span className='text'>{text}</span>
-      {/* Render the Description if it's provided */}
       {Description && <div className='description' >{Description}</div>}
     </Wrapper>
   );
 }
 
-export default TaskInfo;  // Export the TaskInfo component
+export default TaskInfo; 
