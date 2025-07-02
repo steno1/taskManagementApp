21d57f96e3edefe78325e1831208b698aa2c4dc8
Onu Princeley Toochukwu <onuprinceley@gmail.com>
2025-07-02 10:10:57 +0100

    Update AreaChart.js

--- client/src/components/AreaChart.js
+++ client/src/components/AreaChart.js
@@ -1,4 +1,3 @@
-// Import necessary components from the 'recharts' library
 
 import {
   Area,
@@ -12,53 +11,41 @@ import {
 
 import React from 'react';
 
-// Import the 'React' object from the 'react' library
 
-
-// Define a functional component named 'AreaCharts'
-// This component takes a 'data' prop that will contain the data for the area chart
 const AreaCharts = ({ data }) => {
   return (
-    // Create a responsive container to hold the area chart
+   
     <ResponsiveContainer width='100%' height={300}>
-      {/* Define the AreaChart component */}
+ 
       <AreaChart
         data={data}
-        margin={{ top: 50 }} // Margin around the chart
+        margin={{ top: 50 }} 
       >
-        {/* Add a Cartesian grid to the chart */}
         <CartesianGrid strokeDasharray='3 3' />
 
-        {/* Add an X-axis to the chart */}
         <XAxis dataKey='date' />
 
-        {/* Add a Y-axis to the chart */}
         <YAxis allowDecimals={false} />
 
-        {/* Add a tooltip to display information on data points */}
         <Tooltip />
-
-        {/* Use a linear gradient for the fill */}
+  
         <defs>
           <linearGradient id='areaGradient' x1='0' y1='0' x2='0' y2='1'>
-            <stop offset='0%' stopColor='#00B050' stopOpacity={0.8} /> {/* Start color */}
-            <stop offset='100%' stopColor='#00DD6B' stopOpacity={0.2} /> {/* End color */}
+            <stop offset='0%' stopColor='#00B050' stopOpacity={0.8} /> 
+            <stop offset='100%' stopColor='#00DD6B' stopOpacity={0.2} /> 
           </linearGradient>
         </defs>
-
-        {/* Add an area to the chart */}
         <Area
-          type='monotone' // Type of area chart
-          dataKey='count' // Data key for Y-values (count) of the area
-          // Reference the defined gradient fill
+          type='monotone'
+          dataKey='count' 
           fill='url(#areaGradient)'
-          stroke='#00B050' // Border color of the area
-          strokeWidth={2} // Border width
+          stroke='#00B050'
+          strokeWidth={2} 
         />
       </AreaChart>
     </ResponsiveContainer>
   );
 }
 
-// Export the 'AreaCharts' component as the default export of this module
+
 export default AreaCharts;
