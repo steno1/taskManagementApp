7ccb3e901736997d7035f0af50a8861e52ae9aae
Onu Princeley Toochukwu <onuprinceley@gmail.com>
2025-07-02 10:38:28 +0100

    Update StatContainer.js

--- client/src/components/StatContainer.js
+++ client/src/components/StatContainer.js
@@ -1,54 +1,46 @@
-// Importing necessary components and dependencies
 
-import { FaBug, FaCalendarCheck, FaSuitcaseRolling } from 'react-icons/fa'; // Importing icons for statistics
+import { FaBug, FaCalendarCheck, FaSuitcaseRolling } from 'react-icons/fa'; 
+import React from 'react';
+import StatItem from './StatItem';
+import Wrapper from '../assets/wrappers/StatsContainer'; 
+import { useAppContext } from '../context/appContext'; 
 
-import React from 'react'; // Importing React
-import StatItem from './StatItem'; // Importing a component to display each statistic item
-import Wrapper from '../assets/wrappers/StatsContainer'; // Importing a wrapper component
-import { useAppContext } from '../context/appContext'; // Importing the app context for state management
-
-// Defining the StatContainer component
 const StatContainer = () => {
-  // Extracting the stat data from the app context using the useAppContext hook
   const { stat } = useAppContext();
 
-  // Defining an array of default statistic items
   const defaultStats = [
     {
       title: "In-Progress Tasks",
       count: stat.InProgress || 0,
       color: "#BE5A83",
       bcg: "#DEDEA7",
-      icon: <FaSuitcaseRolling /> // Icon representing in-progress tasks
+      icon: <FaSuitcaseRolling /> 
     },
     {
       title: "Completed Tasks",
       count: stat.Completed || 0,
       color: "#617A55",
       bcg: "#F8F6F4",
-      icon: <FaCalendarCheck /> // Icon representing completed tasks
+      icon: <FaCalendarCheck />
     },
     {
       title: "Abandoned Tasks",
       count: stat.Abandoned || 0,
       color: "#7A3E3E",
       bcg: "#9ED2BE",
-      icon: <FaBug /> // Icon representing abandoned tasks
+      icon: <FaBug /> 
     }
   ];
 
-  // Rendering the statistic container
   return (
     <div>
       <Wrapper>
-        {/* Mapping through the defaultStats array to display each StatItem */}
         {defaultStats.map((item, index) => {
-          return <StatItem key={index} {...item} />; // Passing each item's data as props
+          return <StatItem key={index} {...item} />; 
         })}
       </Wrapper>
     </div>
   );
 };
 
-// Exporting the StatContainer component as the default export
 export default StatContainer;
