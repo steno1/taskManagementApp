958066f747d7823691e90cd83168858f7471ac7a
Onu Princeley Toochukwu <onuprinceley@gmail.com>
2025-07-02 10:13:37 +0100

    Update BarChart.js

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -1,4 +1,3 @@
-// Import necessary components from the 'recharts' library
 
 import {
   Bar,
@@ -12,37 +11,24 @@ import {
 
 import React from 'react';
 
-// Import the 'React' object from the 'react' library
-
-
-// Define a functional component named 'BarCharts'
-// This component takes a 'data' prop that will contain the data for the bar chart
 const BarCharts = ({ data }) => {
   return (
-    // Create a responsive container to hold the bar chart
     <ResponsiveContainer width='100%' height={300}>
-      {/* Define the BarChart component */}
       <BarChart
-        // Provide the data for the chart using the 'data' prop
         data={data}
-        // Define margins around the chart
         margin={{ top: 50 }}
       >
-        {/* Add a Cartesian grid to the chart */}
         <CartesianGrid strokeDasharray="3 3" />
-        {/* Add an X-axis to the chart */}
         <XAxis dataKey="date" />
-        {/* Add a Y-axis to the chart */}
         <YAxis allowDecimals={false} />
-        {/* Add a tooltip to display information on data points */}
         <Tooltip />
-        {/* Add a bar to the chart */}
+      
         <Bar
-          // Specify the data key for the Y-values (count) of the bar
+          
           dataKey="count"
-          // Specify the fill color for the bar
+          
           fill="#5B9A8B"
-          // Specify the width/size of the bars
+          
           barSize={75}
         />
       </BarChart>
@@ -50,5 +36,4 @@ const BarCharts = ({ data }) => {
   );
 }
 
-// Export the 'BarCharts' component as the default export of this module
 export default BarCharts;
