c1ee6b97c37741c8ad2c93726c91968e93ed4df5
Onu Princeley Toochukwu <onuprinceley@gmail.com>
2025-07-02 14:15:49 +0100

    Update action.js

--- client/src/context/action.js
+++ client/src/context/action.js
@@ -1,66 +1,48 @@
 
-// Action type constant for displaying an alert
-export const DISPLAY_ALERT = "DISPLAY_ALERT";
 
-// Action type constant for clearing the alert
+export const DISPLAY_ALERT = "DISPLAY_ALERT";
 export const CLEAR_ALERT = "CLEAR_ALERT";
 
-// Action type constants for user registration
 export const REGISTER_USER_BEGIN = "REGISTER_USER_BEGIN";
 export const REGISTER_USER_SUCCESS = "REGISTER_USER_SUCCESS";
 export const REGISTER_USER_ERROR = "REGISTER_USER_ERROR";
 
-// Action type constants for user login
 export const LOGIN_USER_BEGIN = "LOGIN_USER_BEGIN";
 export const LOGIN_USER_SUCCESS = "LOGIN_USER_SUCCESS";
 export const LOGIN_USER_ERROR = "LOGIN_USER_ERROR";
 
-// Action type constants for updating user details
 export const UPDATE_USER_BEGIN = "UPDATE_USER_BEGIN";
 export const UPDATE_USER_SUCCESS = "UPDATE_USER_SUCCESS";
 export const UPDATE_USER_ERROR = "UPDATE_USER_ERROR";
 
-// Action type constant for toggling the sidebar
 export const TOGGLE_SIDEBAR = "TOGGLE_SIDEBAR";
 
-// Action type constant for logging out the user
 export const LOGOUT_USER = "LOGOUT_USER";
-
-// Action type constant for handling form input changes
 export const HANDLE_CHANGE = "HANDLE_CHANGE";
 
-// Action type constant for clearing form input values
 export const CLEAR_VALUES = "CLEAR_VALUES";
 
-// Action type constants for creating a new task
 export const CREATE_TASK_BEGIN = "CREATE_TASK_BEGIN";
 export const CREATE_TASK_SUCCESS = "CREATE_TASK_SUCCESS";
 export const CREATE_TASK_ERROR = "CREATE_TASK_ERROR";
 
-// Action type constants for fetching all tasks
 export const GET_TASK_BEGIN = "GET_TASK_BEGIN";
 export const GET_TASK_SUCCESS = "GET_TASK_SUCCESS";
 
-// Action type constant for setting a task in edit mode
 export const SET_EDIT_TASK = "EDIT_TASK";
 
-// Action type constant for deleting a task
 export const DELETE_TASK_BEGIN = "DELETE_TASK_BEGIN";
 
-// Action type constants for editing a task
 export const EDIT_TASK_BEGIN = "EDIT_TASK_BEGIN";
 export const EDIT_TASK_SUCCESS = "EDIT_TASK_SUCCESS";
 export const EDIT_TASK_ERROR = "EDIT_TASK_ERROR";
 
-// Action type constants for showing statistics
 export const SHOW_STAT_BEGIN = "SHOW_STAT_BEGIN";
 export const SHOW_STAT_SUCCESS = "SHOW_STAT_SUCCESS";
 export const SHOW_STAT_ERROR = "SHOW_STAT_ERROR";
 
-// Action type constant for clearing filters
 export const CLEAR_FILTERS = "CLEAR_FILTERS";
 
-// Action type constant for changing the current page
 export const CHANGE_PAGE = "CHANGE_PAGE";
 
 
