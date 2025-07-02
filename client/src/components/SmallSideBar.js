5fbbbb2ee47b1f184e5545c80667e89cc3f7055e
Onu Princeley Toochukwu <onuprinceley@gmail.com>
2025-07-02 10:33:38 +0100

    Update SmallSideBar.js

--- client/src/components/SmallSideBar.js
+++ client/src/components/SmallSideBar.js
@@ -1,28 +1,22 @@
-// Import required modules and components
 
-import BlueLogo from './logo'; // Import the BlueLogo component from './logo' file
-import { FaTimes } from 'react-icons/fa'; // Import the FaTimes icon from react-icons/fa
+import BlueLogo from './logo'; 
+import { FaTimes } from 'react-icons/fa'; 
 import NavLinks from './NavLinks';
-import React from 'react'; // Import the React library
-import Wrapper from '../assets/wrappers/SmallSidebar'; // Import the Wrapper component from '../assets/wrappers/SmallSidebar' file
-import { useAppContext } from '../context/appContext'; // Import the useAppContext hook from '../context/appContext' file
+import React from 'react';
+import Wrapper from '../assets/wrappers/SmallSidebar'; 
+import { useAppContext } from '../context/appContext'; 
 
-// Define the SmallSideBar component
 const SmallSideBar = () => {
-  // Access shared state and functions from the application context using the useAppContext hook
+ 
   const { showSideBar, toggleSideBar } = useAppContext();
-
-  // Render the SmallSideBar component
   return (
-    // Use the Wrapper component to style the sidebar container
     <Wrapper>
       <div className={showSideBar ? 'sidebar-container show-sidebar' : 'sidebar-container'}>
         <div className='content'>
-          {/* Button to close the sidebar */}
+  
           <button type='button' className='close-btn' onClick={toggleSideBar}>
-            <FaTimes /> {/* Display the FaTimes icon from react-icons/fa */}
+            <FaTimes />
           </button>
-          {/* Header with the BlueLogo component */}
           <header>
             <BlueLogo /> {/* Display the BlueLogo component */}
           </header>
@@ -32,6 +26,4 @@ const SmallSideBar = () => {
     </Wrapper>
   );
 };
-
-// Export the SmallSideBar component as the default export
 export default SmallSideBar;
