a6b3ee33f6c092d5acb7c10d5b47deb53c23137d
Onu Princeley Toochukwu <onuprinceley@gmail.com>
2025-07-02 11:18:31 +0100

    Update StatItem.js

--- client/src/components/StatItem.js
+++ client/src/components/StatItem.js
@@ -1,21 +1,17 @@
-import React from 'react'; // Importing React
-import Wrapper from '../assets/wrappers/StatItem'; // Importing a wrapper component for the StatItem
+import React from 'react'; 
+import Wrapper from '../assets/wrappers/StatItem'; 
 
-// Defining the StatItem component
 const StatItem = ({ count, title, icon, color, bcg }) => {
   return (
-    <Wrapper color={color} bcg={bcg}> {/* Applying the specified color and background color */}
+    <Wrapper color={color} bcg={bcg}> 
       <header>
-        {/* Displaying the count of the statistic */}
+  
         <span className='count'>{count}</span>
-        {/* Displaying the icon associated with the statistic */}
         <span className='icon'>{icon}</span>
       </header>
-      {/* Displaying the title of the statistic */}
       <h5 className='title'>{title}</h5>
     </Wrapper>
   );
 };
 
-// Exporting the StatItem component as the default export
 export default StatItem;
