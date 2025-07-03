3ff156c6978b034675b0e88a0a8214675a4099f4
Onu Princeley Toochukwu <onuprinceley@gmail.com>
2025-07-03 12:48:14 +0100

    Update SharedLayout.js

--- client/src/pages/SharedLayout.js
+++ client/src/pages/SharedLayout.js
@@ -1,4 +1,3 @@
-// Importing required components from the '../components' directory and 'react-router-dom' library.
 
 import { BigSideBar, Navbar, SmallSideBar } from '../components';
 
@@ -6,30 +5,21 @@ import { Outlet } from 'react-router-dom';
 import React from 'react';
 import Wrapper from '../assets/wrappers/SharedLayout';
 
-// Importing the 'React' object from the 'react' library.
-
-
-// Importing a component named 'Wrapper' from the '../assets/wrappers/SharedLayout' directory.
-
-
-// Defining the 'SharedLayout' functional component.
 const SharedLayout = () => {
   return (
-    // Wrapping the entire component in a 'Wrapper' component.
+
     <Wrapper>
       <main className='dashboard'>
-        {/* Rendering the 'SmallSideBar' component. */}
+  
         <SmallSideBar />
-        
-        {/* Rendering the 'BigSideBar' component. */}
+      
         <BigSideBar />
 
         <div>
-          {/* Rendering the 'Navbar' component. */}
           <Navbar />
 
           <div className='dashboard-page'>
-            {/* Rendering the child components defined by the 'Outlet' component. */}
+    
             <Outlet />
           </div>
         </div>
@@ -38,5 +28,4 @@ const SharedLayout = () => {
   );
 };
 
-// Exporting the 'SharedLayout' component as the default export.
 export default SharedLayout;
