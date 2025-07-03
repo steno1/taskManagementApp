f1ccad0be234a6534d2dde08a8761df19a9255dd
Onu Princeley Toochukwu <onuprinceley@gmail.com>
2025-07-03 13:34:24 +0100

    Update BadRequest.js

--- error/BadRequest.js
+++ error/BadRequest.js
@@ -1,15 +1,13 @@
-// Import the CustomApiError class and the StatusCodes module
 
-import CustomApiError from "./CustomApi.js"; // Importing the CustomApiError class for extension
-import { StatusCodes } from "http-status-codes"; // Importing the module for HTTP status codes
+import CustomApiError from "./CustomApi.js"; 
+import { StatusCodes } from "http-status-codes"; 
 
-// Custom error class representing a BadRequestError
 class BadRequestError extends CustomApiError {
     constructor(message) {
-        super(message); // Call the constructor of the extended class (CustomApiError)
+        super(message); 
 
-        this.statusCode = StatusCodes.BAD_REQUEST; // Set the HTTP status code to indicate a bad request
+        this.statusCode = StatusCodes.BAD_REQUEST; 
     }
 }
 
-export default BadRequestError; // Export the BadRequestError class for use in other files
+export default BadRequestError; 
