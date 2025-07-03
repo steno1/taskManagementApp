bd7ed08e1c68f445e15174ecc8dbac0d11df1ff6
Onu Princeley Toochukwu <onuprinceley@gmail.com>
2025-07-03 12:34:59 +0100

    Update AddTask.js

--- client/src/pages/Dashboard/AddTask.js
+++ client/src/pages/Dashboard/AddTask.js
@@ -1,12 +1,10 @@
-// Importing necessary components and context
+
 
 import { Alert } from '../../components';
 import React from 'react';
 import { useAppContext } from '../../context/appContext';
 
-// AddTask component responsible for adding/editing tasks
 const AddTask = () => {
-  // Destructuring state and functions from the context
   const {
     Title,
     Description,
@@ -23,38 +21,31 @@ const AddTask = () => {
     editTask
   } = useAppContext();
 
-  // Function to handle input changes
   const handleTaskInput = (e) => {
     const { name, value } = e.target;
     handleChanges({ name, value });
   };
 
-  // Function to handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Checking for missing Title or Description, and displaying an alert if necessary
     if (!Title || !Description) {
       displayAlert();
       clearAlert();
       return;
     }
 
-    // If editing a task, call the editTask function
     if (isEditing) {
       editTask();
       return;
     }
 
-    // If not editing, create a new task using createTask function
     createTask();
   };
 
   return (
     <div className="add-task-container">
-      {/* Displaying "Edit Task" or "Add Task" based on the editing mode */}
       <h2>{isEditing ? 'Edit Task' : 'Add Task'}</h2>
       <form className="add-task-form" onSubmit={handleSubmit}>
-        {/* Displaying an alert if showAlert is true */}
         {showAlert && <Alert />}
         <div className="form-group">
           <label htmlFor="Title">Title</label>
@@ -105,12 +96,10 @@ const AddTask = () => {
           </select>
         </div>
 
-        {/* Button to add a task, disabled when loading */}
         <button type="submit" onClick={handleSubmit} disabled={isLoading}>
           Add Task
         </button>
 
-        {/* Button to clear input values */}
         <button
           type="button"
           className="clear-button"
