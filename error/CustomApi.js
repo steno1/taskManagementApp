476fa6f6cbfdcf78d30200919f600c3e4216474f
Onu Princeley Toochukwu <onuprinceley@gmail.com>
2025-07-03 13:34:59 +0100

    Update CustomApi.js

--- error/CustomApi.js
+++ error/CustomApi.js
@@ -1,10 +1,8 @@
-// Custom error class representing a base class for API-specific errors
 class CustomApiError extends Error {
    constructor(message) {
-       super(message); // Call the constructor of the extended class (Error)
+       super(message); 
 
-       // Additional custom logic, if needed, can be added here
    }
 }
 
-export default CustomApiError; // Export the CustomApiError class for use in other files
+export default CustomApiError; 
