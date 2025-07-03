5c8979d86a147374f7ac635674c852612830a021
Onu Princeley Toochukwu <onuprinceley@gmail.com>
2025-07-03 13:35:39 +0100

    Update NotFound.js

--- error/NotFound.js
+++ error/NotFound.js
@@ -1,15 +1,13 @@
-// Import the CustomApiError class and the StatusCodes module
 
-import CustomApiError from "./CustomApi.js"; // Importing the CustomApiError class for extension
-import { StatusCodes } from "http-status-codes"; // Importing the module for HTTP status codes
+import CustomApiError from "./CustomApi.js"; 
+import { StatusCodes } from "http-status-codes"; 
 
-// Custom error class representing a NotFoundError
 class notFoundError extends CustomApiError {
     constructor(message) {
-        super(message); // Call the constructor of the extended class (CustomApiError)
+        super(message); 
 
-        this.statusCode = StatusCodes.NOT_FOUND; // Set the HTTP status code to indicate a resource not found
+        this.statusCode = StatusCodes.NOT_FOUND; 
     }
 }
 
-export default notFoundError; // Export the notFoundError class for use in other files
+export default notFoundError; 
