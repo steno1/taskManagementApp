3920b384412db88a68a946352052741a9a01db97
Onu Princeley Toochukwu <onuprinceley@gmail.com>
2025-07-03 13:06:21 +0100

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,43 +1,34 @@
-// Importing necessary error modules
 
 import { BadRequestError, notFoundError, unAuthorizedError } from '../error/index.js';
 
 import { StatusCodes } from 'http-status-codes';
 import User from "../model/user.js";
 
-// Controller for user registration
 const register = async (req, res) => {
-  // Extracting name, email, and password from the request body
+
   const { name, email, password } = req.body;
 
-  // Checking if the name is provided
+
   if (!name) {
     throw new BadRequestError("Please provide a name");
   }
 
-  // Checking if the email is provided
   if (!email) {
     throw new BadRequestError("Please provide an email");
   }
 
-  // Checking if the password is provided
   if (!password) {
     throw new BadRequestError("Please provide a password");
   }
 
-  // Checking if the user with the provided email already exists
   const userAlreadyExist = await User.findOne({ email });
   if (userAlreadyExist) {
     throw new BadRequestError("Email already in use");
   }
-
-  // Creating a new user with the provided data
   const user = await User.create({ name, email, password });
 
-  // Generating a JWT token for the user
   const token = user.createJwt();
 
-  // Sending the response with the created user and token
   res.status(StatusCodes.CREATED).json({
     user: {
       name: user.name,
@@ -48,53 +39,44 @@ const register = async (req, res) => {
   });
 }
 
-// Controller for user login
 const login = async (req, res) => {
   const { email, password } = req.body;
 
-  // Checking if the email is provided
   if (!email) {
     throw new BadRequestError("Please provide an email");
   }
 
-  // Checking if the password is provided
   if (!password) {
     throw new BadRequestError("Please provide a password");
   }
 
-  // Checking if the user with the provided email exists
   const user = await User.findOne({ email }).select("+password");
   if (!user) {
     throw new unAuthorizedError("User does not exist");
   }
 
-  // Comparing the provided password with the stored hashed password
   const isPasswordCorrect = await user.comparePassword(password);
   if (!isPasswordCorrect) {
     throw new unAuthorizedError("Wrong password");
   }
 
-  // Generating a JWT token for the user
   const token = user.createJwt();
 
-  // Clearing the password field in the user object
   user.password = undefined;
 
-  // Sending the response with the user object and token
   res.status(StatusCodes.OK).json({
     user,
     token
   });
 }
 
-// Controller for updating user information
 const update = async (req, res) => {
   const {email, name, lastName}=req.body;
   if(!email || !name || !lastName){
 throw new BadRequestError("Please provide all values")
   }
 
-const user=await User.findOne({_id:req.user.userId})// values from Authenticate.js
+const user=await User.findOne({_id:req.user.userId})
 user.email=email
 user.name=name
 user.lastName=lastName
@@ -108,6 +90,4 @@ res.status(StatusCodes.OK).json({
 })
 
 }
-
-// Exporting the controllers
 export { register, login, update };
